523b8541ddc5af06f4e56d93324d244aa1b62fa4
panduputragit <put.rariadi1144@gmail.com>
2025-02-13 22:44:40 +0800

    store data not permanently

--- lib/event/expense_modal.dart
+++ lib/event/expense_modal.dart
@@ -7,31 +7,58 @@ class ExpenseModal extends StatefulWidget {
 }
 
 class _ExpenseModalState extends State<ExpenseModal> {
+  var enteredTitle = '';
+  var enteredAmount = '';
+  void _saveTitle(String title) {
+    enteredTitle = title;
+  }
+
+  // void _saveAmount(String amount) {
+  //   enteredAmount = amount;
+  // }
+
+  // void _saveDate(String date) {
+  //   print(date);
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitle,
             maxLength: 10,
-            decoration: InputDecoration(labelText: 'Title'),
+            decoration: const InputDecoration(labelText: 'Title'),
           ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Amount'),
-            keyboardType: TextInputType.number,
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Date'),
-            keyboardType: TextInputType.datetime,
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Category'),
-          )
+          // TextField(
+          //   decoration: InputDecoration(labelText: 'Amount'),
+          //   keyboardType: TextInputType.number,
+          // ),
+          // TextField(
+          //   decoration: InputDecoration(labelText: 'Date'),
+          //   keyboardType: TextInputType.datetime,
+          // ),
+          // TextField(
+          //   decoration: InputDecoration(labelText: 'Category'),
+          // )
           // ElevatedButton(
           //   // onPressed: () {},
           //   child: Text('Add Expense'),
           // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(enteredTitle);
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Add Expense'),
+              ),
+            ],
+          )
         ],
       ),
     );
