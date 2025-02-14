e2007af24b190f40456908185242c5b4daa286c6
panduputragit <put.rariadi1144@gmail.com>
2025-02-14 20:41:14 +0800

    view and dubgging

--- lib/event/expense_modal.dart
+++ lib/event/expense_modal.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_app/model/expense_model.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseModal extends StatefulWidget {
@@ -7,19 +8,38 @@ class ExpenseModal extends StatefulWidget {
 }
 
 class _ExpenseModalState extends State<ExpenseModal> {
-  var enteredTitle = '';
-  var enteredAmount = '';
-  void _saveTitle(String title) {
-    enteredTitle = title;
+  // var enteredTitle = '';
+  // void _saveTitle(String title) {
+  //   enteredTitle = title;
+  // }
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  final _dateController = TextEditingController();
+  final _categoryController = TextEditingController();
+  ExpenseCategory _selectedCategory = ExpenseCategory.food;
+
+  void _pickDate() async {
+    await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(0),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      _dateController.text = pickedDate.toString();
+    });
   }
 
-  // void _saveAmount(String amount) {
-  //   enteredAmount = amount;
-  // }
-
-  // void _saveDate(String date) {
-  //   print(date);
-  // }
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    _dateController.dispose();
+    _categoryController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,31 +48,63 @@ class _ExpenseModalState extends State<ExpenseModal> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitle,
+            controller: _titleController,
             maxLength: 10,
             decoration: const InputDecoration(labelText: 'Title'),
           ),
-          // TextField(
-          //   decoration: InputDecoration(labelText: 'Amount'),
-          //   keyboardType: TextInputType.number,
-          // ),
-          // TextField(
-          //   decoration: InputDecoration(labelText: 'Date'),
-          //   keyboardType: TextInputType.datetime,
-          // ),
-          // TextField(
-          //   decoration: InputDecoration(labelText: 'Category'),
-          // )
-          // ElevatedButton(
-          //   // onPressed: () {},
-          //   child: Text('Add Expense'),
-          // ),
+          TextField(
+            controller: _amountController,
+            decoration: const InputDecoration(
+              labelText: 'Amount',
+              prefixText: '\$',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          TextField(
+            controller: _dateController,
+            decoration: const InputDecoration(labelText: 'Date'),
+            onTap: _pickDate,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: ExpenseCategory.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                  // _categoryController.text = value.name;
+                },
+              ),
+            ],
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
-                  print(enteredTitle);
+                  print(_titleController.text);
+                  print(_amountController.text);
+                  print(_dateController.text);
+                  print(_categoryController.text);
                   Navigator.of(context).pop();
                 },
                 child: const Text('Add Expense'),
